76c2600faba9518bc1f8f7774a54f4251f4664bd
Misha Ponizil <misha.ponizil@gmail.com>
2021-07-23 22:10:25 -0700

    Passwordless SMS auth endpoints working \o/

--- web/pages/api/auth/[...nextauth].ts
+++ web/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,32 @@
+// https://github.com/nextauthjs/next-auth/issues/709#issuecomment-784077142
+
+import { padStart, random } from 'lodash'
 import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import Providers, { EmailConfig } from 'next-auth/providers'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { parsePhoneNumber } from 'libphonenumber-js'
 import prisma from '../../../lib/prisma'
+import { twilio, fromPhoneNumber } from '../../../lib/twilio'
+
+const toE164 = (phone: string) => parsePhoneNumber(phone, 'US').format('E.164')
+const generateVerificationToken = () => padStart(`${random(0, 9999)}`, 4, '0')
+
+type EmailOptions = Partial<EmailConfig> & {
+  generateVerificationToken?: () => string
+}
 
 export default NextAuth({
   providers: [
-    Providers.Auth0({
-      clientId: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      domain: process.env.AUTH0_DOMAIN,
-    }),
+    Providers.Email({
+      generateVerificationToken,
+      sendVerificationRequest: async ({ identifier: phone, token }) => {
+        await twilio.messages.create({
+          body: `Your Catch Up login code is ${token}`,
+          from: fromPhoneNumber,
+          to: toE164(phone),
+        })
+      },
+    } as EmailOptions),
   ],
   adapter: PrismaAdapter(prisma),
 })
